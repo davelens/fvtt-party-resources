0f88b1dde0abcd5613c9487612a76041bee6f5bf
davelens <github@davelens.be>
2020-10-25 13:11:56 +0100

    Initial commit.

---
+++ scripts/init.js
@@ -0,0 +1,8 @@
+console.log("Hello World! This code runs immediately when the file is loaded.");
+
+Hooks.on("init", function() {
+  // TODO: Create Firebase document?
+});
+
+Hooks.on("ready", function() {
+});
