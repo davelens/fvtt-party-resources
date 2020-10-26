bae5aa4897f172d2ff0790f0e677531879aa839a
davelens <github@davelens.be>
2020-10-26 10:28:12 +0100

    Add max values to the resources dashboard.

--- scripts/html.js
+++ scripts/html.js
@@ -4,22 +4,27 @@ var dashboard_template = `
   <div class="flexrow">
     <div class="flexcol">
       <p>
-        <span class="value">{{ inspiration }}</span><br>
-        Inspiration
+        <span class="value">{{ inspiration }} / {{ max_inspiration }}</span><br>
+        <span title="TESTING">Inspiration</span>
       </p>
     </div>
     <div class="flexcol">
       <p>
-        <span class="value">{{ desperation }}</span><br>
+        <span class="value">{{ desperation }} / {{ max_desperation }}</span><br>
         Desperation
       </p>
     </div>
     <div class="flexcol">
       <p>
-        <span class="value">{{ influence_dice }}</span><br>
+        <span class="value">{{ influence_dice }} / {{ max_influence_dice }}</span><br>
         Influence Dice
       </p>
     </div>
   </div>
 </section>
 `.trim()
+
+$('body').append(`
+  <script src="https://unpkg.com/@popperjs/core@2"></script>
+  <script src="https://unpkg.com/tippy.js@6"></script>
+`);
