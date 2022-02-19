ff3836be5ad38cea023b5930a207971c0b9baaef
davelens <github@davelens.be>
2022-02-19 20:08:31 +0100

    Add setting toggles for the status bar, and its location.

--- src/resources_status_bar.mjs
+++ src/resources_status_bar.mjs
@@ -1,9 +1,24 @@
+import ModuleSettings from "./settings.mjs";
+
 export default class ResourcesStatusBar {
+  static getData() {
+    return {
+      ...window.pr.api.resources(),
+      ...{
+        is_gm: game.user.isGM,
+        status_bar: ModuleSettings.get('toggle_status_bar')
+      }
+    }
+  }
+
   static async render() {
     const template = 'modules/fvtt-party-resources/src/views/status_bar.html'
-    const status_bar = await renderTemplate(template, window.pr.api.resources())
+    const status_bar = await renderTemplate(template, this.getData())
 
-    $('header#ui-top').prepend(status_bar)
-    $('footer#ui-bottom').append(status_bar)
+    if(ModuleSettings.get('status_bar_location') == 'on_top')
+      $('header#ui-top').prepend(status_bar)
+
+    if(ModuleSettings.get('status_bar_location') == 'at_bottom')
+      $('footer#ui-bottom').append(status_bar)
   }
 }
