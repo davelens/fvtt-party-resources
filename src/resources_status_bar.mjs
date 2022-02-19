ff71eb2ccf69e8eef67b6332104d47cdd523f57a
davelens <github@davelens.be>
2022-02-19 18:55:27 +0100

    Extract the data from the status bar partial into its controller class.

--- src/resources_status_bar.mjs
+++ src/resources_status_bar.mjs
@@ -1,7 +1,15 @@
 export default class ResourcesStatusBar {
   static async render() {
     const template = 'modules/fvtt-party-resources/src/views/status_bar.html'
-    let status_bar = await renderTemplate(template)
+
+    let resources = {
+      skills: [
+        { id: 'silver-bars', name: 'Silver bars', value: '18', icon: 'systems/dnd5e/icons/items/inventory/bar-silver.jpg' },
+        { id: 'gold-bars', name: 'Gold bars', value: '9', icon: 'systems/dnd5e/icons/items/inventory/bar-gold.jpg' }
+      ]
+    }
+
+    let status_bar = await renderTemplate(template, resources)
 
     $('header#ui-top').prepend(status_bar)
     $('footer#ui-bottom').append(status_bar)
