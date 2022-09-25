1caa47c72a4db37446dc2b12d3acb713ccf0c4a3
davelens <github@davelens.be>
2022-09-25 21:40:25 +0200

    Account for all dnd5e currency types (pp,ep,gp,sp, and cp) to calculate the currency reserves in the gold standard.

--- src/actor_resources.mjs
+++ src/actor_resources.mjs
@@ -6,7 +6,7 @@ export default class ActorResources {
   static count(type) {
     switch(type) {
       case 'dnd_fifth_gold_standard':
-        return this.convert_dnd5e_currencies()
+        return this.convert_dnd5e_currencies().toFixed(2)
       default:
         return
     }
@@ -14,8 +14,24 @@ export default class ActorResources {
 
   static convert_dnd5e_currencies() {
     return this.player_characters()
-      .map((item) => { return item.system.currency })
-      .reduce((a,b) => { return a.gp + b.gp })
+      .map((item) => { return this.convert_to_dnd5e_gold(item.system.currency) })
+      .reduce((a,b) => { return a + b })
+  }
+
+  //Coin            CP        SP      EP      GP      PP
+  //Copper (cp)     1         10      50      100     1,000
+  //Silver (sp)     1/10      1       5       10      100
+  //Electrum (ep)   1/50      1/5     1       2       20
+  //Gold (gp)       1/100     1/10    1/2     1       10
+  //Platinum (pp)   1/1,000   1/100   1/20    1/10    1
+  static convert_to_dnd5e_gold(currency_object) {
+    let total = 0.0
+    total += currency_object.cp / 100.0
+    total += currency_object.sp / 10.0
+    total += currency_object.gp
+    total += currency_object.ep * 2.0
+    total += currency_object.pp * 20.0
+    return total
   }
 
   static player_characters() {
