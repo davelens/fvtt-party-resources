7bf1967eecdbd2aa4745a4c1afc4e465ba98c04a
davelens <github@davelens.be>
2026-04-12 16:43:56 +0200

    Refactor status bar's `render()` to replace a pre-existent bar's HTML contents instead of removing/readding the entire bar.

--- src/resources_status_bar.mjs
+++ src/resources_status_bar.mjs
@@ -22,40 +22,72 @@ export default class ResourcesStatusBar {
   }
 
   static async render() {
-    const data = this.getData()
+    // NOTE: I used to do a remove+re-add of the entire bar, but I got reports
+    // of it causing some layout thrashing. So this was expanded into something
+    // that replaces HTML contents of a fresh render into the existing bar.
 
-    const template = 'modules/fvtt-party-resources/src/views/status_bar.html'
-    const status_bar = await foundry.applications.handlebars.renderTemplate(template, data)
+    const existing_bar = $('#fvtt-party-resources-status-bar')
+    const refreshed_render = await foundry.applications.handlebars.renderTemplate(
+      'modules/fvtt-party-resources/src/views/status_bar.html',
+      this.getData()
+    )
+    const new_bar = $(refreshed_render)
+    const location = ModuleSettings.get('status_bar_location')
 
-    $('#fvtt-party-resources-status-bar').remove()
+    if(existing_bar.length) {
+      // Element exists = Update its contents
+      existing_bar.attr('class', new_bar.attr('class'))
+      existing_bar.html(new_bar.html())
+      this._placeInLocation(existing_bar, location)
+    } else if(refreshed_render.trim().length) {
+      // No pre-existent bar = Fresh render
+      this._placeInLocation(new_bar, location)
+      this._bindListeners()
+    }
+  }
 
-    if(ModuleSettings.get('status_bar_location') == 'on_top') {
-      $('#chat-notifications').css('margin-bottom', '0')
-      $('header#ui-top').prepend(status_bar)
+  /**
+   * Move the status bar to the correct parent if it isn't already there.
+   */
+  static _placeInLocation(bar_element, location) {
+    const targets = {
+      on_top:    { parent: $('header#ui-top'),    method: 'prepend', margin: '0' },
+      at_bottom: { parent: $('footer#ui-bottom'), method: 'append',  margin: '30px' }
     }
 
-    if(ModuleSettings.get('status_bar_location') == 'at_bottom') {
-      // Having the actors (or similar) directory listing open, will move the
-      // message textarea to the left, right over the status bar. This moves
-      // the notifications 30px upwards to prevent that.
-      $('#chat-notifications').css('margin-bottom', '30px')
-      $('footer#ui-bottom').append(status_bar)
-    }
+    const target = targets[location]
+    if(!target) return
 
-    $('#fvtt-party-resources-status-bar').click(() => {
+    // Having the actors (or similar) directory listing open, will move the
+    // message textarea to the left, right over the status bar. This moves
+    // the notifications 30px upwards to prevent that.
+    $('#chat-notifications').css('margin-bottom', target.margin)
+
+    if(!target.parent.has(bar_element).length) {
+      bar_element.detach()
+      target.parent[target.method](bar_element)
+    }
+  }
+
+  /**
+   * Bind click listeners once using event delegation so they survive
+   * innerHTML replacements during subsequent renders.
+   */
+  static _bindListeners() {
+    const root = $('#fvtt-party-resources-status-bar')
+
+    root.on('click', () => {
       window.pr.dashboard.render(true, {focus: true})
     })
 
-    $('.fvtt-party-resources-resource').on('click', event => {
-      if (!game.user.isGM || (!event.ctrlKey && !event.metaKey)) {
+    root.on('click', '.fvtt-party-resources-resource', event => {
+      if(!game.user.isGM || (!event.ctrlKey && !event.metaKey))
         return
-      }
 
-      event.stopPropagation()
+      event.stopImmediatePropagation()
 
-      const id = $(event.target).data('id')
-
-      if (!id) {
+      const id = $(event.currentTarget).data('id')
+      if(!id) {
         console.error('Party Resources: Clicked resource does not have an ID!')
         return
       }
